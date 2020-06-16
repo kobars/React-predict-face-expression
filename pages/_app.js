8f91753f3412a458c1c78d26d68d3493ae2fda88
Kobar <kobar@alterra.id>
2020-06-16 17:02:40 +0700

    removed unused css

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import 'react-html5-camera-photo/build/css/index.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
